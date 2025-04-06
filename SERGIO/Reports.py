9e8819fe4e180db8228a79d24f83df63513504c0
manito/creativas <sxnjuxnsergio@gmail.com>
2025-04-06 15:29:39 -0500

    yaya
    holis

---
+++ SERGIO/Reports.py
@@ -0,0 +1,40 @@
+import Query 
+from tabulate import tabulate
+
+def sales():
+    print(tabulate(Query.ventas, headers="keys"))
+
+
+def buscar_x_descripcion():
+    decriptions = []
+    desc = input("QUE PRODUCTO DESEA BUSCAR POR DESCRIPCION: ")
+    for venta in Query.ventas:
+        if venta["DESCRIPTION"].lower() == desc.lower():
+            decriptions.append(venta)
+        
+    if decriptions:
+        print(tabulate(decriptions, headers="keys"))
+    else:
+        print("NO SE ENCONTRO VENTAS CON ESTE NOMBRE")
+
+def buscar_x_fecha():
+     date = []
+     busc = input("QUE PRODUCTO DESEA BUSCAR POR FECHA: ")
+     for vusc in Query.ventas:
+          if vusc["DATE"].lower() == busc.lower():
+               date.append(vusc)
+     if date:
+        print(tabulate(date, headers="keys"))
+     else:
+        print("NO SE ENCONTRO VENTAS CON ESTE NOMBRE")
+
+def buscar_x_comprador():
+     name = []
+     busc = input("QUE PRODUCTO DESEA BUSCAR POR FECHA: ")
+     for vusc in Query.ventas:
+          if vusc["BUYER NAME"].lower() == busc.lower():
+               name.append(vusc)
+     if name:
+        print(tabulate(name, headers="keys"))
+     else:
+        print("NO SE ENCONTRO VENTAS CON ESTE NOMBRE")
