50149746518c57875c52aaa9ac52797bb6e15be2
DarkCode291 <146364431+DarkCode291@users.noreply.github.com>
2025-04-06 13:07:12 -0500

    commit
    i add a diccionarie for sergio

---
+++ SERGIO/Query.py
@@ -0,0 +1,42 @@
+ventas = [
+    {
+        "IdProducto": 101,
+        "dProducto": "Filtro de aceite",
+        "pnProducto": "FO-123",
+        "cvProducto": 2,
+        "sProducto": 48,
+        "nComprador": "Carlos Pérez",
+        "cComprador": 1002457890,
+        "fVenta": "2025-04-01"
+    },
+    {
+        "IdProducto": 102,
+        "dProducto": "Pastillas de freno",
+        "pnProducto": "PF-456",
+        "cvProducto": 1,
+        "sProducto": 99,
+        "nComprador": "Laura Gómez",
+        "cComprador": 1003987654,
+        "fVenta": "2025-04-02"
+    },
+    {
+        "IdProducto": 101,
+        "dProducto": "Filtro de aceite",
+        "pnProducto": "FO-123",
+        "cvProducto": 3,
+        "sProducto": 45,
+        "nComprador": "Andrés Torres",
+        "cComprador": 1005123789,
+        "fVenta": "2025-04-03"
+    },
+    {
+        "IdProducto": 103,
+        "dProducto": "Aceite sintético",
+        "pnProducto": "AS-789",
+        "cvProducto": 5,
+        "sProducto": 20,
+        "nComprador": "Sofía Martínez",
+        "cComprador": 1009876543,
+        "fVenta": "2025-04-04"
+    }
+]
