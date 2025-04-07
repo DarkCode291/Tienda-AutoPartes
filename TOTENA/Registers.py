d2ef77966e874996df691793ba1c4070afa38c74
manito/creativas <sxnjuxnsergio@gmail.com>
2025-04-07 10:43:08 -0500

    commit
    pornhub.com

--- TOTENA/Registers.py
+++ TOTENA/Registers.py
@@ -1,24 +1,55 @@
+import List_Sale
 from tabulate import tabulate
-import TOTENA.List_Sale as List_Sale
-while True:
-    print("-------------------------------------")
-    print("|           AGREGAR VENTA           |")
-    print("-------------------------------------")
+
+def agregar_ventas():
+    while True:
+        print("-------------------------------------")
+        print("|           AGREGAR VENTA           |")
+        print("-------------------------------------")
     
-    venta = {
-        "ID": input("\nID del producto: "),
-        "DES": input("Descripción del producto: "),
-        "NUM PRODUCT": input("Número del producto: "),
-        "AMOUNT": int(input("Cantidad: ")),
-        "STOCK": int(input("Stock disponible: ")),
-        "BUYER NAME": input("Nombre del comprador: "),
-        "CC": int(input("Cédula del comprador: ")),
-        "DATE": input("Fecha (dd/mm/aaaa): ")
-    }
+        venta = {
+            "ID": int(input("ID DEL PRODUCTO: ")),
+            "DES": input("DESCRIPCION DEL PRODUCTO: "),
+            "NUM PRODUCT": input("NUMERO DEL PRODUCTO: "),
+            "AMOUNT": int(input("CANTIDAD: ")),
+            "STOCK": int(input("STOCK DISPONIBLE: ")),
+            "BUYER NAME": input("NOMBRE DEL COMPRADOR: "),
+            "CC": int(input("CEDULA DEL COMPRADOR: ")),
+            "DATE": input("FECHA: (AAA/MM/DD): ")
+        }
 
-    List_Sale.ventas.append(venta)
+        List_Sale.ventas.append(venta)
 
-    if input("QUIERES AGREGAR OTRA VENTA? (escriba 'fin' para salir): ").lower() == "fin":
-        break
+        if input("QUIERES AGREGAR OTRA VENTA? (escriba 'fin' para salir): ").lower() == "fin":
+            break
+    print(tabulate(List_Sale.ventas, headers="keys", tablefmt="fancy_grid"))
 
-print(tabulate(List_Sale.ventas, headers="keys", tablefmt="fancy_grid"))
+
+def cambiar_info():
+    cc_buscado = int(input("INGRESE CC DEL COMPRADOR PARA CAMBIAR INFO"))
+    for i, venta in enumerate(List_Sale.ventas):
+        if venta["CC"] == cc_buscado:
+            print(tabulate([venta], headers="keys", tablefmt="fancy_grid",colalign=("center","center","center","center","center","center","center","center")))
+            confirmar = input("ES ESTA LA VENTA QUE DESEA MODIFICAR? (S/N): ").lower()
+            if confirmar == "s":
+                print("\nINGRESE LA INFO DE LA VENTA ACTUALIZADA:")
+                nueva_venta = {
+                    "ID": int(input("ID DEL PRODUCTO: ")),
+                    "DES": input("DESCRIPCION DEL PRODUCTO: "),
+                    "NUM PRODUCT": input("NUMERO DEL PRODUCTO: "),
+                    "AMOUNT": int(input("CANTIDAD: ")),
+                    "STOCK": int(input("STOCK DISPONIBLE: ")),
+                    "BUYER NAME": input("NOMBRE DEL COMPRADOR: "),
+                    "CC": int(input("CEDULA DEL COMPRADOR: ")),
+                    "DATE": input("FECHA: (AAA/MM/DD): ")
+                }
+                List_Sale.ventas[i] = nueva_venta
+                print("\n VENTA ACTUALIZADA")
+                break
+    else:
+        print("NO SE ENCONTRO LA VENTA.")
+
+    print("\n VENTA ACTUALIZADA:")
+    print(tabulate(List_Sale.ventas, headers="keys", tablefmt="fancy_grid",colalign=("center","center","center","center","center","center","center","center")))
+
+cambiar_info()
